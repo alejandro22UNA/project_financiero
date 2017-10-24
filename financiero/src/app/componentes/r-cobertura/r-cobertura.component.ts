47ef4aa8a6ff22c135ba222897adc8fb6e7cea04
alejandro22UNA <e.morales.ale22@gmail.com>
2017-10-24 15:08:40 -0600

    Implementacion de componentes
    Se agregan los componentes respectivos en el menu debidamente enlazados.

---
+++ financiero/src/app/componentes/r-cobertura/r-cobertura.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-r-cobertura',
+  templateUrl: './r-cobertura.component.html',
+  styleUrls: ['./r-cobertura.component.css']
+})
+export class RCoberturaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
